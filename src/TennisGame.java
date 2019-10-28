121bac26c5993af88bc07d1fc4e8143381368004
zonesama <trannhatthanh31@gmail.com>
2019-10-28 18:06:59 +0700

    First Commmit.

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,22 +1,25 @@
 public class TennisGame {
-
-    public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
+    public static final int love =0;
+    public static final int fiffteen =1;
+    public static final int thirty =2;
+    public static final int fourty =3;
+    public static String getScore(String player1Name, String player2Name, int player1Score, int player2Score) {
         String score = "";
         int tempScore=0;
-        if (m_score1==m_score2)
+        if (player1Score==player2Score)
         {
-            switch (m_score1)
+            switch (player1Score)
             {
-                case 0:
+                case love:
                     score = "Love-All";
                     break;
-                case 1:
+                case fiffteen:
                     score = "Fifteen-All";
                     break;
-                case 2:
+                case thirty:
                     score = "Thirty-All";
                     break;
-                case 3:
+                case fourty:
                     score = "Forty-All";
                     break;
                 default:
@@ -25,9 +28,9 @@ public class TennisGame {
 
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (player1Score>=4 || player2Score>=4)
         {
-            int minusResult = m_score1-m_score2;
+            int minusResult = player1Score-player2Score;
             if (minusResult==1) score ="Advantage player1";
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
@@ -37,20 +40,20 @@ public class TennisGame {
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
+                if (i==1) tempScore = player1Score;
+                else { score+="-"; tempScore = player2Score;}
                 switch(tempScore)
                 {
-                    case 0:
+                    case love:
                         score+="Love";
                         break;
-                    case 1:
+                    case fiffteen:
                         score+="Fifteen";
                         break;
-                    case 2:
+                    case thirty:
                         score+="Thirty";
                         break;
-                    case 3:
+                    case fourty:
                         score+="Forty";
                         break;
                 }
